9be5918b077795881a2da27a7a42b4fc81f36ed4
Leonid Aleksandrov <satellitecross@gmail.com>
2023-05-15 17:58:04 +0300

    fixed loading?

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: { main: './src/pages/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: 'main.js',
     publicPath: ''
   },
   mode: 'development',
